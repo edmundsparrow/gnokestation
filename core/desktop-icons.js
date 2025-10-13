1a5084ae47388a76c47f28d0d8dbf8af9a78ab20
Edmund Sparrow <webaplications5050@gmail.com>
2025-10-13 18:25:59 +0100

    Add files via upload

--- core/desktop-icons.js
+++ core/desktop-icons.js
@@ -1,3 +1,21 @@
+/*
+ * Gnokestation Shell
+ * Copyright (C) 2025 Ekong Ikpe <ekongmikpe@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* ========================================
  * FILE: core/desktop-icons.js
  * VERSION: 1.0.1 (FIXED)
